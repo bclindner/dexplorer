21f2dddd7dcc7ca81b9f31102a7a260ebe14771e
Brian C. Lindner <brian@bclindner.com>
2018-09-27 23:10:56 -0400

    Rewrote and added comments for Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,14 @@
 import styled, { injectGlobal } from 'styled-components'
 
-// neat little flexbox responsive grid
+/**
+ * Object containing Flexbox responsive grid components.
+ */
 export const RG = {}
 
-// simple wrapper class to limit width on large screens & maximize it on small ones
+/**
+ * Wrapper which controls height and width of the app,
+ * and applies some rudimentary styles.
+ */
 export const Wrapper = styled.div`
   font-family: sans-serif;
   margin: 0 auto;
@@ -19,6 +24,9 @@ export const Wrapper = styled.div`
 
 `
 
+/**
+ * Injected body styles that fix some default weirdness. Also applies a background color.
+ */
 export const BodyStyles = injectGlobal`
   body {
     margin: 0 auto;
@@ -27,7 +35,10 @@ export const BodyStyles = injectGlobal`
   }
 `
 
-// not necessarily a 'row' - i use this at least once to display two rows
+/**
+ * A Flexbox responsive 'row'.
+ * Not necessarily a single row, but does force a line break between rows.
+ */
 RG.Row = styled.div`
   display: flex;
   flex-flow: row nowrap;
@@ -36,7 +47,9 @@ RG.Row = styled.div`
   }
 `
 
-// helper class for centering elements on a page (loading spinner!)
+/**
+ * Helper component for displaying horizontally and vertically centered elements inside of a div.
+ */
 RG.Centered = styled.div`
   width: 100%;
   height: 100%;
@@ -47,15 +60,28 @@ RG.Centered = styled.div`
   justify-content: center;
 `
 
+/**
+ * A Flexbox responsive 'column'.
+ * Width can be controlled with the span attribute, 12-column-grid style.
+ * A caveat: due to the nature of Flexbox and this grid configuration, if all elements add up to less than 12 columns, it stretches their width to fit.
+ */
 RG.Col = styled.div`
   flex: ${props => props.span * 8.33}%;
 `
 
+/**
+ * Simple responsive image.
+ * Just sets the width to 100% so it fills the div it's contained in.
+ */
 RG.Image = styled.img`
   box-sizing: border-box;
   width: 100%;
 `
 
+/**
+ * :72,74s/image/table
+ * Also aligns the text left, because table header tags do that by default.
+ */
 RG.Table = styled.table`
   width: 100%;
   text-align: left;
