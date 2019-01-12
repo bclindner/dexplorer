17f6cdcaeef61f62e4aec46452320b935d7ca9c5
Brian C. Lindner <brian@bclindner.com>
2019-01-11 19:02:31 -0500

    Added react-redux, redux-thunk; some work on list reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Dexplorer from './components/Main.js'
 import registerServiceWorker from './registerServiceWorker'
 
+import Dexplorer from './components/Main.js'
+import list from './reducers/list.js'
+
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+let store = createStore(
+  list,
+  applyMiddleware(thunk)
+)
+
 ReactDOM.render(
-  <Dexplorer />
+  <Provider store={store}>
+    <Dexplorer />
+  </Provider>
   , document.getElementById('root'))
 registerServiceWorker()
