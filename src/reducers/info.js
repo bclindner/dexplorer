d8ad580814df5322451852a9813ab8c8075a2032
Brian C. Lindner <brian@bclindner.com>
2019-01-11 22:07:58 -0500

    remapping the PokeAPI response data to be more useful

--- src/reducers/info.js
+++ src/reducers/info.js
@@ -5,11 +5,62 @@ import {
   RECEIVE_VARIANT
 } from '../actions/info.js'
 
+const lang = 'en'
+
 const initialState = {
   ready: false,
   loading: false,
-  species: {},
-  variant: {}
+  info: {
+    pokedexNumber: 0,
+    name: '',
+    genus: '',
+    abilities: [
+      // ''
+    ],
+    height: 0,
+    weight: 0,
+    sprite: '',
+    variants: [
+      // {
+      //    name: '',
+      //    isSelected: ''
+      // }
+    ]
+  },
+  stats: {
+    hp: 0,
+    atk: 0,
+    def: 0,
+    spatk: 0,
+    spdef: 0,
+    spd: 0
+  },
+  effortValues: {
+    hp: 0,
+    atk: 0,
+    def: 0,
+    spatk: 0,
+    spdef: 0,
+    spd: 0
+  },
+  moves: [
+    // {
+    //   name: ''
+    //   versionGroups: [
+    //     name: '',
+    //     level: 0,
+    //     learnedBy: ''
+    //   ]
+    // }
+  ],
+  misc: {
+    eggGroups: [],
+    captureRate: 0,
+    growthRate: '',
+    baseHappiness: 0,
+    baseExp: 0,
+    genderRatio: 0 // percent female (0 is all male, 1 is all female)
+  }
 }
 
 export default function info (state = initialState, action) {
@@ -20,21 +71,90 @@ export default function info (state = initialState, action) {
         loading: true
       }
     case RECEIVE_SPECIES:
-      return {
-        ...state,
-        loading: false,
-        species: action.data
+      // determine pokedex number (if available; if not, return null)
+      let pokedexNumber = null
+      const pokedexEntry = action.data.pokedex_numbers.find(entry => entry.pokedex.name === 'national')
+      if (pokedexEntry !== undefined) {
+        pokedexNumber = pokedexEntry.entry_number
       }
+      // map variants from response
+      const variants = action.data.varieties.map(variant => ({
+        name: variant.pokemon.name,
+        isSelected: variant.is_default
+      }))
+      return {
+        ...initialState,
+        // remain loading because we need to get default variant info before showing it to the user
+        loading: true,
+        info: {
+          ...initialState.info,
+          pokedexNumber,
+          name: action.data.names.find(entry => entry.language.name === lang),
+          genus: action.data.genera.find(entry => entry.language.name === lang),
+          variants
+        },
+        misc: {
+          ...initialState.misc,
+          eggGroups: action.data.egg_groups.map(entry => entry.name),
+          captureRate: action.data.capture_rate,
+          growthRate: action.data.growth_rate.name,
+          baseHappiness: action.data.base_happiness,
+          genderRatio: action.data.gender_rate / 8
+        }
+      }
+      // determine genus
     case REQUEST_VARIANT:
       return {
         ...state,
         loading: true
       }
     case RECEIVE_VARIANT:
+      // stats
+      const hp = action.data.stats.find(entry => entry.stat.name === 'hp')
+      const atk = action.data.stats.find(entry => entry.stat.name === 'attack')
+      const def = action.data.stats.find(entry => entry.stat.name === 'defense')
+      const spatk = action.data.stats.find(entry => entry.stat.name === 'special-attack')
+      const spdef = action.data.stats.find(entry => entry.stat.name === 'special-defense')
+      const speed = action.data.stats.find(entry => entry.stat.name === 'speed')
+      // map moves from response
+      const moves = action.data.moves.map(move => ({
+        name: move.name,
+        versionGroups: move.version_group_details.map(versionGroup => ({
+          name: versionGroup.version_group.name,
+          learnedBy: versionGroup.move_learn_method.name,
+          level: versionGroup.level_learned_at
+        }))
+      }))
       return {
-        ...state,
         loading: false,
-        variant: action.data
+        info: {
+          ...state.info,
+          abilities: action.data.abilities.map(entry => entry.name),
+          height: action.data.height / 10,
+          weight: action.data.weight / 10,
+          sprite: action.data.sprites.front_default
+        },
+        stats: {
+          hp: hp.base_stat,
+          atk: atk.base_stat,
+          def: def.base_stat,
+          spatk: spatk.base_stat,
+          spdef: spdef.base_stat,
+          speed: speed.base_stat
+        },
+        effortValues: {
+          hp: hp.effort,
+          atk: atk.effort,
+          def: def.effort,
+          spatk: spatk.effort,
+          spdef: spdef.effort,
+          speed: speed.effort
+        },
+        moves,
+        misc: {
+          ...state.misc,
+          baseExp: action.data.base_experience
+        }
       }
     default: return state
   }
