7c5fffbcb8ad4a828caf6562505421e44616b126
Brian C. Lindner <brian@bclindner.com>
2019-01-16 11:50:42 -0500

    Styled dropdown menu w/ Material Design icon

--- src/components/StyledLayout.js
+++ src/components/StyledLayout.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components'
 import colors from '../utils/colors'
 import { RG } from './Layout'
+import dropdown from '../dropdown.png'
 
 export const StyledSelect = styled(RG.Select)`
   background-color: ${colors.darker};
   color: ${colors.light};
-  border-radius: 2px;
+  appearance: none;
+  border-radius: 0;
+  border: 1px solid ${colors.dark};
+  background-image: url(${dropdown});
+  background-size: 1em;
+  background-repeat: no-repeat;
+  background-position: 98% center;
+  padding: 0.5em;
+  box-sizing: border-box;
 `
 
 export const SpriteImage = styled(RG.Image)`
